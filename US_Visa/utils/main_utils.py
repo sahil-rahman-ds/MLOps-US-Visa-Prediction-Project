1a6ece00f577aabba47edf1c3c4b51a23a858ee2
sahil-rahman-ds <sahilrahman0827@gmail.com>
2026-08-12 23:05:23 +0530

    Notebook Experiment Completed

--- US_Visa/utils/main_utils.py
+++ US_Visa/utils/main_utils.py
@@ -101,7 +101,7 @@ def save_object(file_path: str, obj: object) -> None:
 def drop_columns(df: DataFrame, cols: list)-> DataFrame:
 
     """
-    drop the columns form a pandas DataFrame
+    drop the columns from a pandas DataFrame
     df: pandas DataFrame
     cols: list of columns to be dropped
     """
