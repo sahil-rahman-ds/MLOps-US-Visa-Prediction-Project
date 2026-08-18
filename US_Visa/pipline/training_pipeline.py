636948f5578877aba80e0602b7a9175b319fc130
sahil-rahman-ds <sahilrahman0827@gmail.com>
2026-08-18 11:44:29 +0530

    Data Transformation and Model Trainer Completed

--- US_Visa/pipline/training_pipeline.py
+++ US_Visa/pipline/training_pipeline.py
@@ -5,18 +5,26 @@ from US_Visa.exception import USvisaException
 
 from US_Visa.components.data_ingestion import DataIngestion
 from US_Visa.components.data_validation import DataValidation
+from US_Visa.components.data_transformation import DataTransformation
+from US_Visa.components.model_trainer import ModelTrainer
 
 from US_Visa.entity.config_entity import (DataIngestionConfig,
-                                          DataValidationConfig)
+                                          DataValidationConfig,
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig)
 
 from US_Visa.entity.artifact_entity import (DataIngestionArtifact,
-                                            DataValidationArtifact)
+                                            DataValidationArtifact,
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
 
@@ -64,6 +72,37 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys) from e
 
+        
+
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data transformation component
+        """
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+
+
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys)
 
     def run_pipeline(self)->None:
         """
@@ -72,6 +111,10 @@ class TrainPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                          data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            
 
         except Exception as e:
             raise USvisaException(e, sys)
